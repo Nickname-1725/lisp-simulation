b116e9d87548f801c02ba991b0af66e6a559c961
Nickname-1725 <nickname-1725@outlook.com>
2024-07-23 21:08:07 +0800

    增加了一个更加复杂的例子，用来帮助整理思路(包含一带阻尼的二阶系统, 内有3个变量)

--- main.lisp
+++ main.lisp
@@ -19,4 +19,31 @@
       (push initial-frame frame-list)
       (handler frame-list n))))
 
+;; dz/dt = y
+;; dy/dt = x
+;; z + y = x
 
+(defun sol-method (initial-frame dt n)
+  (labels ((calc (frame-list dt)
+             (let* ((|frame-z-1| (car frame-list))
+                    (|x(z-1)| (getf |frame-z-1| :x))
+                    (|y(z-1)| (getf |frame-z-1| :y))
+                    (|z(z-1)| (getf |frame-z-1| :z)))
+               ;; 计算导数
+               (let* ((|y'| |x(z-1)|)
+                      (|z'| (- |y(z-1)|))
+                      ;; 积分器进行积分
+                      (y (+ (* dt |y'|) |y(z-1)|))
+                      (z (+ (* dt |z'|) |z(z-1)|)))
+                 ;; 进行帧内计算
+                 (let ((x (+ (* 0.1 y) z)))
+                   ;; 构造新的帧
+                   `(:x ,x :y ,y :z ,z)))))
+           (handler (frame-list n)
+             (cond
+               ((eql n 0) frame-list)
+               (t (push (calc frame-list dt) frame-list)
+                  (handler frame-list (1- n))))))
+    (let (frame-list)
+      (push initial-frame frame-list)
+      (handler frame-list n))))
