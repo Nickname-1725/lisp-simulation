335a2dcc1e71174b07519328ccef6a4d1efa5664
Nickname-1725 <nickname-1725@outlook.com>
2024-07-26 22:49:00 +0800

    分离出一个积分器，使计算过程更加通用化

--- main.lisp
+++ main.lisp
@@ -24,26 +24,38 @@
 ;; z + y = x
 
 (defun sol-method (initial-frame dt n)
-  (labels ((calc (frame-list dt)
+  (labels ((integrator (frame-list d-list dt)
+             "积分器，根据导数和之前的值计算现在的值"
+             (let ((|frame(z-1)| (car frame-list))
+                   (|d(z-1)| (car d-list)))
+               (let* ((|y(z-1)| (getf |frame(z-1)| :y))
+                      (|z(z-1)| (getf |frame(z-1)| :z))
+                      (|y'| (getf |d(z-1)| :y))
+                      (|z'| (getf |d(z-1)| :z)))
+                 (let ((y (+ (* dt |y'|) |y(z-1)|))
+                       (z (+ (* dt |z'|) |z(z-1)|)))
+                   (values y z)))))
+           (calc (frame-list d-list dt)
+             "计算器，进行一帧的计算"
              (let* ((|frame-z-1| (car frame-list))
                     (|x(z-1)| (getf |frame-z-1| :x))
-                    (|y(z-1)| (getf |frame-z-1| :y))
-                    (|z(z-1)| (getf |frame-z-1| :z)))
+                    (|y(z-1)| (getf |frame-z-1| :y)))
                ;; 计算导数
                (let* ((|y'| |x(z-1)|)
-                      (|z'| (- |y(z-1)|))
-                      ;; 积分器进行积分
-                      (y (+ (* dt |y'|) |y(z-1)|))
-                      (z (+ (* dt |z'|) |z(z-1)|)))
-                 ;; 进行帧内计算
-                 (let ((x (+ (* 0.1 y) z)))
-                   ;; 构造新的帧
-                   `(:x ,x :y ,y :z ,z)))))
-           (handler (frame-list n)
+                      (|z'| (- |y(z-1)|)))
+                 ;; 积分器进行积分
+                 (push `(:y ,|y'| :z ,|z'|) d-list)
+                 (multiple-value-bind (y z) (integrator frame-list d-list dt)
+                   ;; 进行帧内计算
+                   (let ((x (+ (* 0.1 y) z)))
+                     ;; 构造新的帧
+                     `(:x ,x :y ,y :z ,z))))))
+           (handler (frame-list d-list n)
+             "管理器，使计算器计算指定帧数"
              (cond
                ((eql n 0) frame-list)
-               (t (push (calc frame-list dt) frame-list)
-                  (handler frame-list (1- n))))))
-    (let (frame-list)
+               (t (push (calc frame-list d-list dt) frame-list)
+                  (handler frame-list d-list (1- n))))))
+    (let (frame-list d-list)
       (push initial-frame frame-list)
-      (handler frame-list n))))
+      (handler frame-list d-list n))))
