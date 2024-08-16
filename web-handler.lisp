99329a7724a166016857f76848250aad6127676a
Nickname-1725 <nickname-1725@outlook.com>
2024-08-16 22:34:08 +0800

    加入了一个绘图尝试，能够正常在前端页面上显示

---
+++ web-handler.lisp
@@ -0,0 +1,34 @@
+
+(defparameter *acc* (make-instance 'hunchentoot:easy-acceptor :port 8080))
+(setf (hunchentoot:acceptor-document-root *acc*) #P"./www/") ; 前端主页, 使用index.html文件
+
+(hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
+  (setf (hunchentoot:content-type*) "text/plain")
+  (format nil "Hey~@[ ~A~]!" name))
+
+(hunchentoot:define-easy-handler (swing-tree :uri "/pendulum"
+                                             :default-request-type :post)
+    ()
+  (let* (;(frame (hunchentoot:parameter "frame")) ; 获取链接中"?var=0"格式的数据
+         ;(name (hunchentoot:parameter "name"))
+         ;(frame (read-from-string frame))
+         ;(json-data (hunchentoot:raw-post-data :force-text t))
+         ;(config (jonathan:parse json-data))
+         ;(s-t (make-instance 'swing-tree))
+         )
+    ;(config-class-attr s-t config) ; 根据请求传入的数据配置摇摆树参数
+    (let* (;(json-data (jonathan:to-json (swing-tree-animate s-t frame)))
+           (result '(((:|line| (:|x1| 1000 :|y1| 600 :|x2| 1000 :|y2| 250
+                                :|width| 40.0))
+                      (:|line| (:|x1| 100 :|y1| 0 :|x2| 100 :|y2| 100 :|width| 1)))
+                     ((:|line| (:|x1| 1000 :|y1| 600 :|x2| 1000 :|y2| 250
+                                :|width| 40.0))
+                      (:|line| (:|x1| 100 :|y1| 0 :|x2| 100 :|y2| 100 :|width| 1)))))
+           ;(result (demo-pendulum* nil))
+           (json-data (jonathan:to-json result)))
+      ;(dump-json name json-data)
+      json-data)))
+
+(defun init-fun ()
+  (hunchentoot:start *acc*)
+  (loop while t do (sleep 1))) ; 死循环防止可执行文件立刻退出
